fd7411a913c9a5eabc83e92b4493e6a5b998a2ec
redpanda-cat <leungs1@mskcc.org>
2021-03-10 14:15:32 -0800

    Add render file for NDV

---
+++ src/NDV/render.py
@@ -0,0 +1,37 @@
+from jinja2 import Template
+import subprocess
+import pandas as pd
+import sys
+import os
+import json
+import shutil
+
+
+data_dir = sys.argv[1]
+
+metadata = pd.read_csv(os.path.join(data_dir, "metadata.tsv"), sep="\t")
+metadata = metadata.to_dict('records')
+
+probabilities = pd.read_csv(os.path.join(data_dir, "probabilities.tsv"), sep="\t")
+probabilities = probabilities.to_dict("records")
+
+data = {
+    "metadata": metadata,
+    "probabilities": probabilities
+}
+
+data = json.dumps(data, indent=4)
+
+app_dir = os.path.dirname(os.path.abspath(__file__))
+app_dir = os.path.abspath(os.path.join(app_dir, "../..", "build"))
+index_template=os.path.join(app_dir, "index.html")
+template = Template(open(index_template,"r").read())
+
+
+html = template.render(data=data)
+output_html=os.path.join(app_dir,"NDV.html")
+output = open(output_html,"w")
+output.write(html)
+output.close()
+# datalake_build=os.path.join(sys.argv[1], "build")
+# shutil.copytree(build_folder, datalake_build)
