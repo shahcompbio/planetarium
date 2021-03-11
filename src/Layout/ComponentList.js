5af9abe3733b648397cbb4e2dd9f64490a8f1d7a
vbojilova <bojilovv@mskcc.org>
2021-03-11 12:15:37 -0500

    spelling mistake

--- src/Layout/ComponentList.js
+++ src/Layout/ComponentList.js
@@ -1,6 +1,6 @@
 import Umap from "../UMAP/Umap.js";
 import Heatmap from "../Heatmap/Heatmap.js";
-import SubTypeUmap from "../UMAP/subTypeUmap.js";
+import SubTypeUmap from "../UMAP/SubtypeUmap.js";
 import Barplot from "../Barplot/Barplot.js";
 
 const componentList = {
