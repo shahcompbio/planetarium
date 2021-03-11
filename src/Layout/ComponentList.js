4c7284c665401528d261bbe4fcc1eae144190881
redpanda-cat <leungs1@mskcc.org>
2021-03-11 09:46:14 -0800

    bug fix

--- src/Layout/ComponentList.js
+++ src/Layout/ComponentList.js
@@ -1,12 +1,12 @@
 import Umap from "../UMAP/Umap.js";
 import Heatmap from "../Heatmap/Heatmap.js";
-import SubTypeUmap from "../UMAP/SubtypeUmap.js";
+import SubTypeUmap from "../UMAP/subTypeUmap.js";
 import Barplot from "../Barplot/Barplot.js";
 
 const componentList = {
   UMAP: Umap,
   HEATMAP: Heatmap,
   SUBTYPEUMAP: SubTypeUmap,
-  BARPLOT: Barplot
+  BARPLOT: Barplot,
 };
 export default componentList;
