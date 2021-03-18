1dfbb61d2d6a41d3113137d1f4320116b6f48ece
redpanda-cat <leungs1@mskcc.org>
2021-03-18 09:59:20 -0700

    Prelim refactoring Barplot

--- src/Barplot/Barplot.js
+++ src/Barplot/Barplot.js
@@ -1,24 +1,182 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import * as d3 from "d3";
 import _ from "lodash";
 import { useDashboardState } from "../PlotState/dashboardState";
 
-import { canvasInit, changeFontSize } from "../DrawingUtils/utils.js";
+import { useCanvas } from "../components/utils/useCanvas";
 
-const Barplot = ({ data, chartDim }) => {
+import { changeFontSize } from "../DrawingUtils/utils.js";
+
+/*
+
+Stacked bar chart of proportions (so they all add to 100%)
+
+*/
+
+const BAR_COLORS = [
+  "#5E4FA2",
+  "#3288BD",
+  "#66C2A5",
+  "#ABDDA4",
+  "#E6F598",
+  "#FFFFBF",
+  "#FEE08B",
+  "#FDAE61",
+  "#F46D43",
+  "#D53E4F",
+  "#9E0142",
+];
+
+const DataWrapper = ({ data, chartDim }) => {
   const [{ clonotypeParam, subtypeParam, fontSize }] = useDashboardState();
 
-  const [drawReady, setDrawReady] = useState(false);
-  const [context, saveContext] = useState(null);
+  const groupedSubtype = _.groupBy(data, subtypeParam);
+  const subtypes = Object.keys(groupedSubtype).sort();
+
+  const countedClonotypes = subtypes.reduce((countMap, subtype) => {
+    const clonotypeCount = _.countBy(groupedSubtype[subtype], clonotypeParam);
+
+    const countFreq = _.countBy(Object.values(clonotypeCount), (value) =>
+      Math.min(value, 10)
+    );
+
+    return { ...countMap, [subtype]: countFreq };
+  }, {});
+
+  console.log(countedClonotypes);
+  return (
+    <StackedBarProportion
+      data={countedClonotypes}
+      chartDim={chartDim}
+      barValues={Array.from(Array(10).keys())}
+    />
+  );
+};
+
+const StackedBarProportion = ({ data, chartDim, barValues }) => {
+  // make this flexible
+  const categoryValues = Object.keys(data);
+
+  const chartWidth = chartDim["width"];
+  const chartHeight = chartDim["height"] - 50;
+
+  const catScale = d3
+    .scaleBand()
+    .domain(categoryValues)
+    .range([0, chartWidth])
+    .paddingInner(0.2);
+
+  const barScale = d3
+    .scaleLinear()
+    .domain([0, 1])
+    .range([0, chartHeight]);
+
+  const colors = d3
+    .scaleOrdinal()
+    .domain(barValues)
+    .range(BAR_COLORS.slice(0, barValues.length));
+
+  const ref = useCanvas(
+    (canvas) => {
+      const context = canvas.getContext("2d");
+
+      drawBars(
+        context,
+        data,
+        categoryValues,
+        barValues,
+        catScale,
+        barScale,
+        colors
+      );
+      // drawLegend(context);
+      // drawBars(context);
+      // drawAxisLabels(context);
+      // drawYAxisLabels(context);
+    },
+    chartDim["width"],
+    chartDim["height"],
+    []
+  );
+
+  return (
+    <div
+      style={{
+        width: chartDim["width"],
+        height: chartDim["height"],
+        position: "relative",
+      }}
+    >
+      <div
+        id="barchart"
+        style={{
+          position: "absolute",
+          pointerEvents: "all",
+          display: "flex",
+        }}
+      >
+        <canvas ref={ref} />
+      </div>
+    </div>
+  );
+};
+
+const drawBars = (
+  context,
+  data,
+  categoryValues,
+  barValues,
+  catScale,
+  barScale,
+  colors
+) => {
+  categoryValues.map((cValue) => {
+    const categoryData = data[cValue];
+    const total = Object.values(categoryData).reduce((sum, x) => sum + x, 0);
+
+    var currHeight = barScale(1);
+    const xPos = catScale(cValue);
+
+    barValues.map((bValue) => {
+      if (categoryData.hasOwnProperty(bValue)) {
+        context.fillStyle = colors(bValue);
+        const barHeight = barScale(categoryData[bValue] / total);
+
+        console.log(
+          currHeight,
+          barHeight,
+          bValue,
+          categoryData[bValue] / total,
+          barScale(categoryData[bValue] / total)
+        );
+        context.fillRect(
+          xPos,
+          currHeight - barHeight,
+          catScale.bandwidth(),
+          barHeight
+        );
+
+        currHeight += barHeight * -1;
+      }
+    });
+  });
+};
+
+const StackedBar = ({ data, chartDim }) => {
+  const [{ clonotypeParam, subtypeParam, fontSize }] = useDashboardState();
+
   const barWidth = 50;
   const groupedData = _.groupBy(data, subtypeParam);
+  // console.log(groupedData);
   const subtypes = Object.keys(groupedData).sort();
   const stackedBarData = subtypes.reduce((final, subtype) => {
     const groupedClonotypes = _.groupBy(groupedData[subtype], clonotypeParam);
+    // console.log(_.countBy(groupedData[subtype], clonotypeParam));
     var counter = {};
     const hitList = Object.entries(groupedClonotypes).map(
       (cellHits) => cellHits[1].length
     );
+    // console.log(hitList);
     hitList.forEach((x) => (counter[x] = (counter[x] || 0) + 1));
     final[subtype] = {
       total: hitList.length,
@@ -27,6 +185,7 @@ const Barplot = ({ data, chartDim }) => {
     return final;
   }, []);
 
+  // console.log(stackedBarData);
   // X axis
   const x = d3
     .scaleBand()
@@ -41,19 +200,20 @@ const Barplot = ({ data, chartDim }) => {
   var colors = d3
     .scaleOrdinal()
     .domain([...Array.from(Array(10).keys())])
-    .range([
-      "#5E4FA2",
-      "#3288BD",
-      "#66C2A5",
-      "#ABDDA4",
-      "#E6F598",
-      "#FFFFBF",
-      "#FEE08B",
-      "#FDAE61",
-      "#F46D43",
-      "#D53E4F",
-      "#9E0142",
-    ]);
+    .range(BAR_COLORS);
+
+  const ref = useCanvas(
+    (canvas) => {
+      const context = canvas.getContext("2d");
+      drawLegend(context);
+      drawBars(context);
+      drawAxisLabels(context);
+      drawYAxisLabels(context);
+    },
+    chartDim["width"],
+    chartDim["height"],
+    []
+  );
 
   function drawBars(context) {
     context.beginPath();
@@ -116,20 +276,6 @@ const Barplot = ({ data, chartDim }) => {
       5
     );
   }
-  useEffect(() => {
-    if (data.length > 0) {
-      init(data, chartDim);
-    }
-  }, [data]);
-  useEffect(() => {
-    if (drawReady) {
-      //  drawAxis(context, x, y, chartDim["chart"]);
-      drawLegend(context);
-      drawBars(context);
-      drawAxisLabels(context);
-      drawYAxisLabels(context);
-    }
-  }, [drawReady]);
 
   function drawYAxisLabels(context) {
     context.fillStyle = "black";
@@ -169,37 +315,26 @@ const Barplot = ({ data, chartDim }) => {
       context.restore();
     });
   }
-  function init(data, chartDim) {
-    var canvas = d3.select("#barplotCanvas");
-    var currContext = canvasInit(canvas, chartDim.width, chartDim.height);
-
-    currContext.fillStyle = "white";
-    currContext.fillRect(0, 0, chartDim.width, chartDim.height);
-    saveContext(currContext);
-    setDrawReady(true);
-  }
 
   return (
-    <div>
+    <div
+      style={{
+        width: chartDim["width"],
+        height: chartDim["height"],
+        position: "relative",
+      }}
+    >
       <div
+        id="barchart"
         style={{
-          width: chartDim["width"],
-          height: chartDim["height"],
-          position: "relative",
+          position: "absolute",
+          pointerEvents: "all",
+          display: "flex",
         }}
       >
-        <div
-          id="barchart"
-          style={{
-            position: "absolute",
-            pointerEvents: "all",
-            display: "flex",
-          }}
-        >
-          <canvas id="barplotCanvas" />
-        </div>
+        <canvas ref={ref} />
       </div>
     </div>
   );
 };
-export default Barplot;
+export default StackedBar;
