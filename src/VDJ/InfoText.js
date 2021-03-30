9953884602ad06e7df9eabf58ba1ca073fefa986
redpanda-cat <leungs1@mskcc.org>
2021-03-30 15:38:34 -0700

    Remove br tags from infotext

--- src/VDJ/InfoText.js
+++ src/VDJ/InfoText.js
@@ -2,12 +2,12 @@ const infoText = {
   UMAP: {
     title: "Cdr3 Amino Acid Sequence UMAP",
     text:
-      "UMAP embedding generated from <br>the subset of CD8/CD4 T cells<br> identified with CellAssign. <br>The top 10 TRB sequences by <br>frequency are mapped to cells <br>in the embedding. The radius slider <br>allows the user to highlight areas <br>of high density for a given sequence <br>that can be referenced against<br> the subtype UMAP.",
+      "UMAP embedding generated from the subset of CD8/CD4 T cells identified with CellAssign. The top 10 TRB sequences by frequency are mapped to cells in the embedding. The radius slider allows the user to highlight areas of high density for a given sequence that can be referenced against the subtype UMAP.",
   },
   SUBTYPEUMAP: {
     title: "Subtype UMAP",
     text:
-      "UMAP embedding generated from<br> the subset of CD8/CD4 T-cells<br> identified with CellAssign. <br>Subtypes are assigned from differential gene expression<br> derived from Leiden clustering. <br>The list of current subtypes and known gene markers<br> is provided below.",
+      "UMAP embedding generated from the subset of CD8/CD4 T-cells identified with CellAssign. Subtypes are assigned from differential gene expression derived from Leiden clustering. The list of current subtypes and known gene markers is provided below.",
   },
   HEATMAP: {
     title: "Subtype to Sequence Heatmap",
@@ -17,17 +17,17 @@ const infoText = {
   HISTOGRAM: {
     title: "Generation Probabilities",
     text:
-      "Distribution of log10 generation probabilities <br>generated from OLGA for all TRB sequences.<br>Description of the OLGA method can be found here: <br>https://academic.oup.com/bioinformatics<br>/article/35/17/2974/5292315/. <br>The log10 probability of each top 10 TRB sequence <br>can be referenced with respect to the entire distribution<br> of the sample. Sequences with a lower probability of <br>generation will be found in the left tail of the distribution.",
+      "Distribution of log10 generation probabilities generated from OLGA for all TRB sequences. Description of the OLGA method can be found here: https://academic.oup.com/bioinformatics/article/35/17/2974/5292315/. The log10 probability of each top 10 TRB sequence can be referenced with respect to the entire distribution of the sample. Sequences with a lower probability of generation will be found in the left tail of the distribution.",
   },
   BARPLOT: {
     title: "Clonotype Expansion",
     text:
-      "Visualization of clonal expansion in each subtype<br> by plotting the fraction of cells that belong to sequences <br>expanded to N cells. The fraction of cells with sequences<br> that belong to a single cell (labeled 1 in legend) describes<br> the relative proportion of unexpanded clonotypes. <br>The fraction of cells with sequences that belong <br>to >10 cells describes the relative proportion of highly<br> expanded clonotypes within the subtype.",
+      "Clonal expansion in each subtype by plotting the fraction of cells that belong to sequences expanded to N cells. The fraction of cells with sequences that belong to a single cell (labeled 1 in legend) describes the relative proportion of unexpanded clonotypes. The fraction of cells with sequences that belong to >10 cells describes the relative proportion of highly expanded clonotypes within the subtype.",
   },
   TABLE: {
     title: "Differentially Expressed Genes",
     text:
-      "Differentially expressed genes for each subtype <br>(1 vs. All) using the Wilcoxon test. The top 50 <br>statistically significant (p < 0.001) with a minimum<br> log fold change of 0.25 and expression <br>found in >= 50% of the subtype cells are included<br> for each subtype. It is possible to sort DEGs by both<br> p-value and log fold change.",
+      "Differentially expressed genes for each subtype (1 vs. All) using the Wilcoxon test. The top 50 statistically significant (p < 0.001) with a minimum log fold change of 0.25 and expression found in >= 50% of the subtype cells are included for each subtype. It is possible to sort DEGs by both p-value and log fold change.",
   },
 };
 export default infoText;
