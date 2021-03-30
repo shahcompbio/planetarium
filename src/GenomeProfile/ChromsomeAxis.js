508d0a68e9a6faa7b27a380d403621dd29058b5f
bestnewkevin <kn1120@nyu.edu>
2021-03-30 16:51:11 -0400

    adding genomeProfile

---
+++ src/GenomeProfile/ChromsomeAxis.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useD3 } from "./useD3";
+import { Y_AXIS_WIDTH } from "./utils";
+
+const ChromosomeAxis = ({ chromosomes, bpTotal, width, height }) => {
+  const ref = useD3((svg) => {
+    const backgroundColors = ["#fefefe", "#eee"];
+    const bpRatio = width / bpTotal;
+
+    svg.style("transform", `translate(${Y_AXIS_WIDTH}px, 0px)`);
+
+    svg
+      .append("g")
+      .attr("class", "chromosome-axis-boxes")
+      .selectAll(".chromosome-axis-box")
+      .data(chromosomes)
+      .enter()
+      .append("rect")
+      .attr("class", (d) => "chromosome-axis-box chrom-" + d.chr)
+      .attr("x", (d) => d.genomeStart * bpRatio)
+      .attr("y", 0)
+      .attr("width", (d) => d.length * bpRatio)
+      .attr("height", height)
+      .attr("fill", (d, i) => backgroundColors[i % 2]);
+
+    svg
+      .append("g")
+      .attr("class", "chromosome-axis-texts")
+      .selectAll(".chromosome-axis-text")
+      .data(chromosomes)
+      .enter()
+      .append("text")
+      .attr("class", (d) => "chromosome-axis-text chrom-" + d.chr)
+      .style("fill", "black")
+      .attr("x", (d) => (d.genomeStart + d.length / 2) * bpRatio)
+      .attr("y", height)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "10px")
+      .text((d) => d.chr);
+  });
+  return <svg id="chromosome-axis" ref={ref} width={width} height={height} />;
+};
+
+export default ChromosomeAxis;
