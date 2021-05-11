4ef81c1d30b3119863dc5137c359d8fa7ed7e51a
vbojilova <bojilovv@mskcc.org>
2021-05-11 13:46:26 -0400

    chord diagram

---
+++ src/Circos/Circos.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef } from "react";
+import Layout from "../Layout/Layout";
+import _ from "lodash";
+import * as d3 from "d3";
+import "./App.css";
+import dashboardReducer, { initialState } from "../PlotState/dashboardReducer";
+import { DashboardProvider } from "../PlotState/dashboardState";
+
+import Network from "../components/Chord/Chord";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+import { theme } from "../theme/theme.js";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const Circos = ({ data }) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <DashboardProvider
+        initialState={{
+          ...initialState
+        }}
+        reducer={dashboardReducer}
+      >
+        <Grid
+          container
+          direction="column"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          <Grid style={{ margin: 15 }}>
+            <Network
+              chartName={"CIRCOS"}
+              data={data}
+              chartDim={{
+                height: 800,
+                width: 950
+              }}
+            />
+          </Grid>
+        </Grid>
+      </DashboardProvider>
+    </MuiThemeProvider>
+  );
+};
+
+export default Circos;
