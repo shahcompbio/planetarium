35d8da3b4774457456d2a9bf671d09ac65b33ea2
redpanda-cat <leungs1@mskcc.org>
2021-05-13 12:41:00 -0700

    Update package components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 //bar
-export { default as ProbabilityHistogram } from './Bar/ProbabilityHistogram'
-export { default as StackedHorizontalBar } from './Bar/StackedHorizontalBar'
+export { default as ProbabilityHistogram } from "./Bar/ProbabilityHistogram";
+export { default as StackedHorizontalBar } from "./Bar/StackedHorizontalBar";
 //genomeProfile
-export { default as GenomeProfile } from './GenomeProfile/GenomeProfile'
-//copynumberheatmap
-
-export { default as CopyNumberHeatmap } from './CopyNumberHeatmap/CopyNumberHeatmap'
-export { default as CopyNumberHeatmapGenomeProfile } from './CopyNumberHeatmapGenomeProfile/CopyNumberHeatmapGenomeProfile'
+export { default as GenomeProfile } from "./CopyNumber/Profile";
 //heatmap
-export { default as Heatmap } from './Heatmap/Heatmap'
+export { default as Heatmap } from "./Heatmap/Heatmap";
 //infoBar
-export { default as InfoBar } from './InfoBar/InfoBar'
-export { default as Layout } from './InfoBar/Layout'
+export { default as InfoBar } from "./InfoBar/InfoBar";
+export { default as Layout } from "./InfoBar/Layout";
 
-export { default as VerticalLegend } from './Legend/VerticalLegend'
+export { default as VerticalLegend } from "./Legend/VerticalLegend";
 
-export { useCanvas } from './utils/useCanvas'
-export { useD3 } from './utils/useD3'
+export { useCanvas } from "./utils/useCanvas";
+export { useD3 } from "./utils/useD3";
 
-export { isValueHighlighted, isHighlighted } from './utils/isHighlighted'
+export { isValueHighlighted, isHighlighted } from "./utils/isHighlighted";
