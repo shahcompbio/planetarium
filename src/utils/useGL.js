6c88fb71034533d95e9c2936a2088f66f2cd3ef1
viki <viki.bojilova@gmail.com>
2022-01-31 11:52:44 -0800

    Adding use gl util

---
+++ src/utils/useGL.js
@@ -0,0 +1,30 @@
+import { useRef, useCallback } from "react";
+import * as d3 from "d3";
+
+export function useGL(width, height, id = "umapCanvas") {
+  const ref = useRef(null);
+  const setRef = useCallback(
+    (node) => {
+      if (ref.current) {
+      }
+
+      if (node) {
+        const canvas = node.appendChild(document.createElement("canvas"));
+
+        canvas.id = id;
+
+        const context = canvas.getContext("webgl");
+        let scale = window.devicePixelRatio;
+        canvas.style.width = width + "px";
+        canvas.style.height = height + "px";
+        canvas.width = width * scale;
+        canvas.height = height * scale;
+      }
+
+      ref.current = node;
+    },
+    [width, height]
+  );
+
+  return [setRef];
+}
